a0bd1d091a4b8ce2bfd94263d30388b8ae0b4be0
suman chapai <sumanchapai@gmail.com>
2025-03-24 23:23:25 +0545

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+Simple reverse proxy that sits infront of multiple services and choses a service
+to serve request based on the request URL path.
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"strings"
+)
+
+func main() {
+	gitProxyURL, err := url.Parse("http://127.0.0.1:7001")
+	if err != nil {
+		log.Fatal(err)
+	}
+	gitProxy := httputil.NewSingleHostReverseProxy(gitProxyURL)
+
+	appProxyURL, err := url.Parse("http://127.0.0.1:5000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	appProxy := httputil.NewSingleHostReverseProxy(appProxyURL)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if strings.HasPrefix(r.URL.Path, "/git/") {
+			gitProxy.ServeHTTP(w, r)
+			return
+		}
+
+		if r.URL.Path == "/git" {
+			http.Redirect(w, r, "/git/", http.StatusPermanentRedirect)
+			return
+		}
+
+		appProxy.ServeHTTP(w, r)
+	})
+
+	fmt.Println("Server listening on :7002")
+	log.Fatal(http.ListenAndServe(":7002", nil))
+}
